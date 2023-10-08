66082da43c6dde8268fc622d3bb520b1c230be1f
Andre Antunes <andrelfantunes@gmail.com>
2023-10-08 18:36:44 -0300

    Client-side demo

---
+++ src/utils/bgColors.ts
@@ -0,0 +1,13 @@
+export const bgColors: string[] = [
+  "#e9b198",
+  "#e595a2",
+  "#e6d2b7",
+  "#cbd4b7",
+  "#a6d1b5",
+  "#c3d7f4",
+  "#f1c3e7",
+  "#9f645e",
+  "#e49086",
+  "#c6a0ee",
+  "#a0d6ec",
+];
